8513dd15cbce16e8d0d108780838a83f6e562766
wardahkhurram <googli3.@gmail.com>
2024-04-23 12:51:37 +0500

    first commit

---
+++ first-day.js
@@ -0,0 +1,8 @@
+// -------Question-1----------
+var person_name = "Asharib";
+console.log("Hello ".concat(person_name, ", would you like to learn some TypeScript today?"));
+// -------Question-2----------
+var NAME = "gul noor";
+console.log(NAME.toUpperCase());
+console.log(NAME.toLowerCase());
+console.log(NAME.charAt(0).toUpperCase() + NAME.slice(1).toLowerCase());
---
+++ first-day.ts
@@ -0,0 +1,12 @@
+// -------Question-1----------
+
+let person_name :string = "Asharib";
+console.log(`Hello ${person_name}, would you like to learn some TypeScript today?`);
+
+
+// -------Question-2----------
+
+let NAME:string = "gul noor";
+console.log(NAME.toUpperCase());
+console.log(NAME.toLowerCase());
+console.log(NAME.charAt(0).toUpperCase() + NAME.slice(1).toLowerCase()); 
